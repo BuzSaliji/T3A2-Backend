ed3450a54f5908a3ba24c59e26c8c36f9a2d40c0
Burim Saliji <buz.saliji@gmail.com>
2023-11-30 16:40:07 +1100

    Set up UserAuthFunction

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,7 @@
 // import Express library
 const express = require('express');
+const { user } = require('../models/UserModel');
+const { comparePassword, generateJwt } = require('../functions/userAuthFunctions');
 
 // make an instance of a Router
 const router = express.Router();
@@ -8,25 +10,47 @@ const router = express.Router();
 
 
 router.get("/", async (request, response) => {
+    let result = await User.find({});
 
+	response.json({result});
 })
 
 // GET localhost:3000/users/laijhjsdaljfdhbsal
 router.get("/:id", async (request, response) => {
+    let result = await User.findOne({_id: request.params.id});
 
+	response.json({result});
 })
 
 // POST localhost:3000/users/
 router.post("/", async (request, response) => {
+    let newUser = await User.create(request.body).catch(error => error);
 
+	response.json(newUser);
 })
 
 // POST localhost:3000/users/login
 // request.body = {username: "admin", password: "Password1"}
 // respond with {jwt: "laskdnalksfdnal;fgvkmsngb;sklnmb", valid: true}
 router.post("/login", async (request, response) => {
+        // Find user by provided username 
+	let targetUser = await User.findOne({username: request.body.username}).catch(error => error);
 
-})
+	// Check if user provided the correct password
+	let isPasswordCorrect = await comparePassword(request.body.password, targetUser.password);
+
+	if (!isPasswordCorrect){
+		response.status(403).json({error:"You are not authorised to do this!"});
+	}
+
+	// If they provided the correct, generate a JWT
+	let freshJwt = generateJwt(targetUser._id.toString());
+
+	// respond with the JWT 
+	response.json({
+		jwt: freshJwt
+	});
+});
 
 // GET localhost:3000/users/verify
 // JWT in request.headers["jwt"] or request.headers["authorization"]
