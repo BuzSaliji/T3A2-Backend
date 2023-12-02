400a91d4426d85e63f3c8ffaf0da115a34fc1995
Burim Saliji <buz.saliji@gmail.com>
2023-12-03 06:31:26 +1100

    Update Booking Controller

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -1,28 +1,71 @@
 const express = require('express');
-const { Booking } = require('../models');
+const { Booking } = require('../models/BookingModel');
+const authMiddleware = require('../middleware/authMiddleware'); // Assuming you have authentication middleware
 
 const router = express.Router();
 
+// Middleware to authenticate and authorize users
+router.use(authMiddleware);
+
+// Get all bookings
 router.get('/', async (req, res) => {
-  const bookings = await Booking.findAll();
-  res.json(bookings);
+  try {
+    const bookings = await Booking.find();
+    res.json(bookings);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/', async (req, res) => {
-    const booking = await Booking.create(req.body);
-    res.json(booking);
-});
-
+// Get a specific booking by ID
 router.get('/:id', async (req, res) => {
-    const booking = await Booking.findByPk(req.params.id);
+  try {
+    const booking = await Booking.findById(req.params.id);
+    if (!booking) {
+      return res.status(404).json({ message: 'Booking not found' });
+    }
     res.json(booking);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+// Create a new booking
+router.post('/', async (req, res) => {
+  try {
+    // Add logic to check for court availability, user eligibility, etc.
+    const newBooking = new Booking(req.body);
+    await newBooking.save();
+    res.status(201).json(newBooking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Update a booking
 router.patch('/:id', async (req, res) => {
-    await Booking.update(req.body, {
-        where: { id: req.params.id }
-    });
-    res.json({ success: 'Booking updated' });
+  try {
+    const updatedBooking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedBooking) {
+      return res.status(404).json({ message: 'Booking not found' });
+    }
+    res.json(updatedBooking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Delete a booking
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedBooking = await Booking.findByIdAndDelete(req.params.id);
+    if (!deletedBooking) {
+      return res.status(404).json({ message: 'Booking not found' });
+    }
+    res.json({ message: 'Booking deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
